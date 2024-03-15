6821dd3eebfa1a815880f5f97dc12008d61be376
wajhiulhassan <wajhi.512minhas@gmail.com>
2024-03-15 23:45:41 +0500

    first commit

---
+++ calculator-script.js
@@ -0,0 +1,12 @@
+function appendToScreen(value) {
+    document.getElementById('screen').value += value;
+}
+
+function clearScreen() {
+    document.getElementById('screen').value = '';
+}
+
+function calculate() {
+    var result = eval(document.getElementById('screen').value);
+    document.getElementById('screen').value = result;
+}
